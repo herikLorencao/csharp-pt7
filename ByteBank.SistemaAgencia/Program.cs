39b08a1933c8dc74d682b193edcc914a1d00f7f0
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-21 13:31:06 -0300

    adicionando arrays

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,26 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            int[] idades = new int[]
+            {
+                10,
+                20,
+                30,
+                40,
+                50
+            };
+
+            int acumulador = 0;
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.WriteLine($"Idade do indice {i}: {idades[i]}");
+                acumulador += idades[i];
+                Console.WriteLine(idades[i]);
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"A média das idades é {media} anos");
             Console.ReadLine();
         }
     }
